8fb00b1026e8659b9ff68b27634c23664bab7372
Aldhaneka <aldhanekadev@gmail.com>
2020-11-21 04:17:35 +0700

    initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author')
+
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+
+    if (req.query.name) {
+        searchOptions.name = new RegExp(req.query.name.trim(), 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', { authors: authors, input: req.query });
+    } catch {
+        res.redirect('/');
+    }
+});
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+})
+
+// create author
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name.trim()
+    });
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render("authors/new", { author: author, errorMessage: "error has occured" });
+    }
+})
+
+module.exports = router;
